8f8b08a45c7750de0f8b30fb63deb16a060d913d
bipoliaro4ka <doctor.who.rip@gmail.com>
2024-02-26 11:13:44 +0700

    Всё

--- bboard/main/admin.py
+++ bboard/main/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
 
-# Register your models here.
+from main.forms import ApplicationForm
+from main.models import *
+
+
+class ApplicationAdmin(admin.ModelAdmin):
+    form = ApplicationForm
+    list_display = ('name', 'date', 'username', 'status')
+    fields = ('status', 'img', 'comment')
+    list_filter = ('status', 'categories')
+
+
+admin.site.register(Categorise)
+admin.site.register(Application, ApplicationAdmin)
+admin.site.register(User)
